e2b49aec183580775498221550d2d894fce078ba
maliefrr <aliefm144@gmail.com>
2022-05-19 21:10:06 +0700

    add kobo kanaeru gif and make hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+      container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
+      colors: {
+        primary: "#0EA5E9",
+      },
       fontFamily: { 
         poppins: ["Poppins", "sans-serif"],
-      }
+      },
+      screen: {
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
